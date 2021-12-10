a2515b8a58d53ec696be71fb4432f385bfc3e5da
Sean Morgan <insideseanshead@gmail.com>
2021-12-09 20:32:45 -0800

    added data and list components to render cards"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+
 function App() {
-    return (
-        <div className='container'>
-            <h1>My App</h1>
-        </div>
-    )
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
