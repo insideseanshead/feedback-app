7a857d95b9c10dcfceb4420803cb9f2e2846490a
Sean Morgan <insideseanshead@gmail.com>
2021-12-09 21:24:07 -0800

    added delte function

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,16 +1,27 @@
-import FeedbackItem from "./FeedbackItem"
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({ feedback }) {
-    if(!feedback || feedback.length === 0) {
-        return <p>No feedback yet</p>
-    }
-    return (
-        <div className='feedbacl-list'>
-           {feedback.map((item)=>(
-               <FeedbackItem key={item.id} item={item} />
-           ))}
-        </div>
-    )
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No feedback yet</p>;
+  }
+  return (
+    <div className="feedbacl-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
 }
 
-export default FeedbackList
+FeedbackList.proptyples = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FeedbackList;
