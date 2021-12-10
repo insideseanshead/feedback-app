f58e81ebf85af5277986cc7d0c22fcbf4785541f
Sean Morgan <insideseanshead@gmail.com>
2021-12-09 21:41:49 -0800

    reinstalled dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -16,6 +18,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
