3988020764b38f4e9672d71def684e5b1d681b10
Sean Morgan <insideseanshead@gmail.com>
2021-12-21 13:06:04 -0800

    created shared button component

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,26 +1,34 @@
-import React from 'react'
-import { useState } from 'react'
-import Card from './shared/Card'
+import React from "react";
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
 
 export const FeedbackForm = () => {
-    const [text,setText] = useState('')
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState("true");
+  const [message, setSetMessage] = useState("");
 
-    const handleTextChange = (e) => {
-        setText(e.target.value)
-    }
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
 
-    return (
-        <Card>
-            <form>
-                <h2>How would you rate your service?</h2>
-                {/* TODO: rating select component */}
-                <div className='input-group'>
-                    <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text} />
-                    <button type='submit'>Send</button>
-                </div>
-            </form>
-        </Card>
-    )
-}
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service?</h2>
+        {/* TODO: rating select component */}
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" >Send</Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
 
-export default FeedbackForm
+export default FeedbackForm;
