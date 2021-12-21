b4a6ae4ccee8fe07594985d7d255c94b62fa267b
Sean Morgan <insideseanshead@gmail.com>
2021-12-21 13:40:29 -0800

    can now add rating and feedback

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -2,21 +2,48 @@ import React from "react";
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
-export const FeedbackForm = () => {
+export const FeedbackForm = ({handleAdd}) => {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState("true");
-  const [message, setSetMessage] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if(text === '') {
+        setBtnDisabled(true)
+        setMessage(null)
+    } else if(text !== '' && text.trim().length <= 10) {
+        setMessage('Text must be at least 10 characters')
+        setBtnDisabled(true)
+    } else {
+        setMessage(null)
+        setBtnDisabled(false)
+    }
+
     setText(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      if (text.trim().length > 10) {
+          const newFeedback = {
+              text,
+              rating
+          }
+
+          handleAdd(newFeedback)
+
+          setText('')
+      }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service?</h2>
-        {/* TODO: rating select component */}
+        <RatingSelect select={setRating} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -24,8 +51,10 @@ export const FeedbackForm = () => {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit" >Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
